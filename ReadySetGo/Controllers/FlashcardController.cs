2ada7385a39a90f79c05294ee5b18e8f629e407a
Eivind Omtveit <eivind@oivi.co>
2023-11-28 11:52:13 +0100

    prøver å legge til første flashcardside

--- ReadySetGo/Controllers/FlashcardController.cs
+++ ReadySetGo/Controllers/FlashcardController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReadySetGo.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +14,25 @@ namespace ReadySetGo.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var flashcards = new List<Flashcard>();
+
+            var flashcard1 = new Flashcard
+            {
+                Front = "Sannhet",
+                Back = "Truth"
+            };
+
+            var flashcard2 = new Flashcard
+            {
+                Front = "Mot",
+                Back = "Courage"
+            };
+
+            flashcards.Add(flashcard1);
+            flashcards.Add(flashcard2);
+
+            ViewBag.CurrentViewName = "List of flashcards";
+            return View(flashcards);
         }
     }
 }
---
+++ ReadySetGo/Models/Flashcard.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ReadySetGo.Models
+{
+	public class Flashcard
+	{
+		public string Front { get; set; }
+		public string Back { get; set; }
+	}
+}
+
